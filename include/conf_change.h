46f885e1075f4f4a22b70f1f239cbab839cad159
zhuichao001 <zhuichao001@163.com>
2021-12-04 13:06:50 +0800

    repath code

--- include/conf_change.h
+++
@@ -1,5 +0,0 @@
-
-struct ConfChange{
-    Peer peer;
-    int type = 0; //0 invalid, 1 ADD,  -1 REMOVE
-};
--- include/raft_fsm.h
+++
@@ -1,7 +0,0 @@
-#include<string>
-
-class RaftFSMInterface {
-public:
-    virtual Status Apply(std::string) = 0;
-    virtual uint64_t GetAppliedIndex() = 0;
-};
